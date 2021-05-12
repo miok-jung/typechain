905ff76652be3f29f8c86b8b96ece4668d4a9d52
miok-jung <miok.dev@gmail.com>
2021-05-13 00:10:14 +0900

    Settings tsc-watch

---
+++ README.md
@@ -0,0 +1,3 @@
+Date : 21. 05. 12
+
+[Notion TypeScript](https://www.notion.so/TypeScript-1461f9e3f6214abd86e05908aa3dfcce)
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Helo ${name}, you are ${age}, you are a ${gender}!!!`;
+};
+console.log(sayHi("Jung Mi Ok", 444, "Female"));
+export {};
