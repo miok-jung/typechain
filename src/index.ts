0fe1f1f8e18727a5c1d3e011958e56d3b364031b
miok-jung <miok.dev@gmail.com>
2021-05-17 21:35:55 +0900

    study typescript class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Helo ${name}, you are ${age}, you are a ${gender}!!!`;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const narin = new Human("Narin", 19, "female");
+
+const sayHi = (person: Human): string => {
+  return `Helo ${person.name}, you are ${person.age}, you are a ${person.gender}!!!`;
 };
-console.log(sayHi("Jung Mi Ok", 444, "Female"));
+console.log(sayHi(narin));
 export {};
